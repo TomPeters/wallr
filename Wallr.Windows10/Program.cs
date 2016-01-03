9ea3bd99560a96517dc388544363e38fd9833d29
Tom Peters <tom@tjpeters.net>
2016-01-03 19:18:41 +1000

    Added a system tray icon. The icon itsely needs replacing.

--- Wallr.Windows10/Program.cs
+++ Wallr.Windows10/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Wallr.Windows10
 {
@@ -10,6 +7,8 @@ namespace Wallr.Windows10
     {
         static void Main(string[] args)
         {
+            WallrApplicationContext applicationContext = new WallrApplicationContext();
+            Application.Run(applicationContext);
         }
     }
 }
---
+++ Wallr.Windows10/WallrApplicationContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Wallr.Windows10
+{
+    public class WallrApplicationContext : ApplicationContext
+    {
+        private NotifyIcon _notifyIcon;
+
+        public WallrApplicationContext()
+        {
+            InitializeNotifyIcon();
+        }
+
+        private void InitializeNotifyIcon()
+        {
+            var components = new Container();
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            contextMenuStrip.Items.Add(new ToolStripButton("Exit", null, exitItem_Click));
+            _notifyIcon = new NotifyIcon(components)
+            {
+                ContextMenuStrip = contextMenuStrip,
+                Visible = true,
+                Icon = new Icon(Assembly.GetEntryAssembly().GetManifestResourceStream("Wallr.Windows10.assets.wallr.ico")), // TODO: Update icon
+                Text = "Wallr"
+            };
+        }
+
+        private void exitItem_Click(object sender, EventArgs e)
+        {
+            ExitThread();
+        }
+
+        protected override void ExitThreadCore()
+        {
+            _notifyIcon.Visible = false;
+            base.ExitThreadCore();
+        }
+    }
+}
