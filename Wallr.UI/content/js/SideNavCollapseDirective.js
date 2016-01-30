73a22c8f7590e137fd911bc1f44e3b3cae92a2ba
Tom Peters <tom@tjpeters.net>
2016-01-30 17:04:19 +1000

    Nav bar and side bar working.

---
+++ Wallr.UI/content/js/SideNavCollapseDirective.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+angular.module("Wallr").directive("sideNavCollapse", function() {
+    return {
+        restruct: 'A',
+        scope: false,
+        link: function (scope, iElement, iAttrs, controller, transcludeFn) {
+            $(iElement).sideNav();
+        }
+    }
+})
