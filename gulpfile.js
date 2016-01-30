62d82de00fb15efddc94909055c8957262bb83bb
Tom Peters <tom@tjpeters.net>
2016-01-31 08:06:08 +1000

    Added live reload to gulp. Added default task to gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
 
-gulp.task('watch-content', function() {
+gulp.task('default', ['watch-content']);
+
+gulp.task('watch-content', function () {
+    livereload.listen();
     return gulp.src('Wallr.UI/content/**/*')
         .pipe(watch('Wallr.UI/content/**/*'))
-        .pipe(gulp.dest('Wallr.Windows10/bin/Debug/Content'));
+        .pipe(gulp.dest('Wallr.Windows10/bin/Debug/Content'))
+        .pipe(livereload());
 });
 
