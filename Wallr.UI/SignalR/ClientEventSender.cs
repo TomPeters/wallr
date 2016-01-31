1f60d07672c49e71b33d6b5caf81425215492895
Tom Peters <tom@tjpeters.net>
2016-01-31 11:44:56 +1000

    Also send wallpaper update events to the client.

--- Wallr.UI/SignalR/ClientEventSender.cs
+++ Wallr.UI/SignalR/ClientEventSender.cs
@@ -14,23 +14,31 @@ namespace Wallr.UI.SignalR
     {
         private readonly IImageStreamUpdateEvents _imageStreamUpdateEvents;
         private readonly IHubContext _hubContext;
+        private readonly IWallpaperUpdateEvents _wallpaperUpdateEvents;
         private List<IDisposable> _eventSubscriptions;
 
-        public ClientEventSender(IImageStreamUpdateEvents imageStreamUpdateEvents, IHubContext hubContext)
+        public ClientEventSender(IImageStreamUpdateEvents imageStreamUpdateEvents, IHubContext hubContext, IWallpaperUpdateEvents wallpaperUpdateEvents)
         {
             _imageStreamUpdateEvents = imageStreamUpdateEvents;
             _hubContext = hubContext;
+            _wallpaperUpdateEvents = wallpaperUpdateEvents;
         }
 
         public void StartSendingEvents()
         {
             _eventSubscriptions = new List<IDisposable>();
             _eventSubscriptions.Add(_imageStreamUpdateEvents.ImageStreamUpdateRequested.Subscribe(i =>
-                SendEvent("ImageStreamUpdate", new
-                    {
-                        NumberOfImagesRequested = i
-                    })
-                ));
+            SendEvent("ImageStreamUpdate", new
+                {
+                    NumberOfImagesRequested = i
+                })
+            ));
+            _eventSubscriptions.Add(_wallpaperUpdateEvents.UpdateImageRequested.Subscribe(i =>
+                SendEvent("WallpaperUpdate", new
+                {
+                    TimeSinceLastUpdate = i
+                })
+            ));
         }
 
         private void SendEvent(string eventName, object eventArgs)
