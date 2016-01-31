6b38b852035d1f68b4e66f726c05f6e8070552ff
Tom Peters <tom@tjpeters.net>
2016-01-31 11:30:26 +1000

    Extracted events service and added rx to the project.

---
+++ Wallr.UI/content/js/Events.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+var wallrModule = angular.module("Wallr");
+
+wallrModule.factory('events', function() {
+    return {
+        startHub: function() {
+            $.connection.hub.url = "http://localhost:29485/signalr";
+            $.connection.wallrHub.client.sendEvent = function (eventName, eventArgs) {
+                console.log(eventName);
+                console.log(eventArgs);
+            };
+            $.connection.hub.start();
+        }
+    }
+});
+
+wallrModule.run(['events', function(events) {
+    events.startHub();
+}]);
