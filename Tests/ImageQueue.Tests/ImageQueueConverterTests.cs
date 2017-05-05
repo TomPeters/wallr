afe71c1d6d75525df84e3774a869fa9fdf84c4b9
Tom Peters <tom@tjpeters.net>
2017-05-05 13:07:05 +1000

    Added serialization tests for the image queue converter.

---
+++ Tests/ImageQueue.Tests/ImageQueueConverterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Wallr.ImagePersistence;
+using Wallr.ImageQueue;
+using Wallr.ImageQueue.Persistence;
+using Wallr.ImageSource;
+using Xunit;
+
+namespace ImageQueue.Tests
+{
+    public class ImageQueueConverterTests
+    {
+        [Fact]
+        public void RoundTrip_QueueValuesPreserved()
+        {
+            IEnumerable<SourceQualifiedImageId> queuedIds = new[]
+            {
+                CreateImageId("1"),
+                CreateImageId("2"),
+                CreateImageId("3")
+            };
+
+            var sut = new ImageQueueConverter(new SourceQualifiedImageIdConverter());
+
+            var json = sut.Serialize(queuedIds);
+            sut.Deserialize(json).Should().Equal(queuedIds);
+        }
+
+        private SourceQualifiedImageId CreateImageId(string imageId)
+        {
+            return new SourceQualifiedImageId(new ImageSourceId(Guid.NewGuid()), new ImageId(imageId));
+        }
+    }
+}
