62bf91e71f024343e1744ae1b73793ce9bcfae9a
Tom Peters <tom@tjpeters.net>
2017-05-05 10:14:56 +1000

    Added ImageQueue.Tests project and tests for the SourceQualifiedImageIdConverter

---
+++ Tests/ImageQueue.Tests/SourceQualifiedImageIdConverterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using Wallr.ImagePersistence;
+using Wallr.ImageQueue.Persistence;
+using Wallr.ImageSource;
+using Xunit;
+
+namespace ImageQueue.Tests
+{
+    public class SourceQualifiedImageIdConverterTests
+    {
+        [Fact]
+        public void RoundTrip_ValuesAreEquivalent()
+        {
+            var sut = new SourceQualifiedImageIdConverter();
+            var id = new SourceQualifiedImageId(new ImageSourceId(Guid.NewGuid()), new ImageId("some image id"));
+            var serializationModel = sut.ToSerializationModel(id);
+            sut.FromSerializationModel(serializationModel).Should().Be(id);
+        }
+    }
+}
